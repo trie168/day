9a04c26c388b912bacc07714dab301a38c822054
trie168 <trie168@gmail.com>
2020-01-13 13:17:28 +0700

    update

--- models/book.model.js
+++ models/book.model.js
@@ -1,14 +1,22 @@
-const mongoose  = require('mongoose')
-const Schema    = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-let bookSchema  = new Schema ({
-    bookId      : Number,
-    title       : String,
-    author      : String,
-    year        : String,
-    publisher   : String
-})
+let bookSchema = new Schema({
+    bookId: Number,
+    title: String,
+    author: String,
+    year: String,
+    publisher: String,
+    created_at: {
+        type: Date,
+        default: Date.now
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now
+    }
+});
 
-let Book = mongoose.model("Book", bookSchema)
+let Book = mongoose.model("Book", bookSchema);
 
-module.exports = Book
+module.exports = Book;
--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,6 +1,6 @@
-const express   = require('express')
-const router    = express.Router()
-const bookModel = require('../models/book.model')
+const express = require("express");
+const router = express.Router();
+const bookModel = require("../models/book.model");
 
 // bookId     : Number,
 // title       : String,
@@ -8,40 +8,72 @@ const bookModel = require('../models/book.model')
 // year        : String,
 // press       : String
 
-router.post('/create', async (req, res) => {
-    let { bookId, title, author, year, publisher } = req.body
+router.post("/create", async (req, res) => {
+    let { bookId, title, author, year, publisher } = req.body;
     let inputData = {
-        bookId, title, author, year, publisher
-    }
+        bookId,
+        title,
+        author,
+        year,
+        publisher
+    };
 
-    let data = new bookModel(inputData)
-    data.save()
+    let data = new bookModel(inputData);
+    data.save();
 
     return res.send({
         status: "Success",
         message: "The information of book has submited",
-        data 
-        })
-})
+        data
+    });
+});
 
-router.get('/getAll', async(req,res) => {
-    let result = await bookModel.find({}).exec()
+router.get("/getAll", async (req, res) => {
+    let result = await bookModel.find({}).exec();
 
     res.send({
-        status: 'Success',
-        message: 'Those are the result.',
+        status: "Success",
+        message: "Those are the result.",
         result
-    })
-})
+    });
+});
 
-router.get('/:id', async(req,res) => {
-    let { id } = req.params
-    let data = await bookModel.findOne({ _id:id }).exec()
+router.get("/:id", async (req, res) => {
+    let { id } = req.params;
+    let data = await bookModel.findOne({ _id: id }).exec();
 
     return res.send({
-        status: 'Success',
+        status: "Success",
         message: "Here is your request",
         data
-        })
-})
-module.exports = router
+    });
+});
+
+router.delete("/:id", async (req, res) => {
+    let { id } = req.params;
+    let data = await bookModel.findOneAndDelete({ _id: id }).exec();
+
+    return res.send({
+        status: "Success",
+        message: "delete success",
+        data
+    });
+});
+
+router.put("/:id", async (req, res) => {
+    let { id } = req.params;
+    let { bookId, title, author, year, publisher } = req.body;
+
+    let updated_data = { bookId, title, author, year, publisher };
+
+    let data = await bookModel
+        .findOneAndUpdate({ _id: id }, updated_data)
+        .exec();
+
+    return res.send({
+        status: "Success",
+        message: "update success",
+        data
+    });
+});
+module.exports = router;
